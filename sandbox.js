a1836c56635031eddb8735f09c353dda078b5a83
Mac <mac@Macs-MacBook-Pro.local>
2024-07-10 18:09:25 +0100

    typescript on object and arrays

--- sandbox.js
+++ sandbox.js
@@ -5,3 +5,23 @@ isBlackbelt = true;
 var num = 8;
 var circ = function (diameter) { return diameter * Math.PI; };
 console.log(circ(5));
+var names = ['lois', 'mike', 'james'];
+// names.push('vicky');
+// names.push(3);
+console.log(names);
+var mixed = ['ken', 6, 9, 'chun', true];
+mixed.push('ryu');
+mixed.push(89);
+mixed[0] = 5;
+console.log(mixed);
+// objects
+var ninja = {
+    name: 'mario',
+    belt: 'belt',
+    age: 45
+};
+ninja.age = 70;
+ninja.name = 'rya';
+ninja.age = 6;
+console.log(ninja)
+
--- sandbox.ts
+++ sandbox.ts
@@ -10,3 +10,26 @@ let num = 8;
 
 const circ = (diameter : number) => diameter * Math.PI
 console.log(circ(5))
+
+let names = ['lois', 'mike', 'james'];
+// names.push('vicky');
+// names.push(3);
+
+console.log(names);
+
+let mixed = ['ken' , 6, 9, 'chun', true];
+mixed.push('ryu');
+mixed.push(89);
+mixed[0] = 5;
+console.log(mixed);
+
+// objects
+
+let ninja = {
+    name: 'mario',
+    belt: 'belt',
+    age: 45
+};
+
+ninja.age = 70;
+ninja.name = 'rya';
