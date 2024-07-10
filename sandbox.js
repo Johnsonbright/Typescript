063bce3366b1cfb78da71085bbb06cdd44011be7
Mac <mac@Macs-MacBook-Pro.local>
2024-07-10 17:39:11 +0100

    first commit

---
+++ sandbox.js
@@ -0,0 +1,7 @@
+var character = 'mario';
+var age = 30;
+var isBlackbelt = false;
+isBlackbelt = true;
+var num = 8;
+var circ = function (diameter) { return diameter * Math.PI; };
+console.log(circ(5));
---
+++ sandbox.ts
@@ -0,0 +1,12 @@
+let character = 'mario';
+
+let age = 30;
+
+let isBlackbelt = false;
+
+isBlackbelt = true;
+
+let num = 8;
+
+const circ = (diameter : number) => diameter * Math.PI
+console.log(circ(5))
